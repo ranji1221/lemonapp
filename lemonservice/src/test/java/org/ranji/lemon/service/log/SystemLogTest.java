f4a495cbefbec3f72d06ee1e083857e2198f238d
Administrator <Administrator@BOBO>
2017-09-21 15:13:54 +0800

    添加日志实体类的Dao和Service层

---
+++ lemonservice/src/test/java/org/ranji/lemon/service/log/SystemLogTest.java
@@ -0,0 +1,95 @@
+package org.ranji.lemon.service.log;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ranji.lemon.model.log.SystemLog;
+import org.ranji.lemon.service.log.prototype.ISystemLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the"License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+ * See the License for the specific language governing permissions and limitations under the License.
+ * Copyright [2017] [RanJi] [Email-jiran1221@163.com]
+ * 
+ * Authority模块中的SystemLogService测试类
+ * @author LiJianBo
+ * @date 2017-9-21
+ * @since JDK1.7
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:spring-persist.xml","classpath:spring-service.xml"})
+public class SystemLogTest {
+	
+	@Autowired
+	private ISystemLogService systemLogService;
+
+	//存储日志测试方法
+		@Test
+		public void testAddSystemLog(){
+			for(int i = 11; i < 20; i++){
+				SystemLog systemLog = new SystemLog();
+				systemLog.setLogType("警告" + i);
+				systemLog.setLogTitle("访问首页");
+				systemLog.setRemoteAddr("192.168.1.16");
+				systemLog.setRequestUri("192.168.1.16");
+				systemLog.setMethod("post");
+				systemLog.setParams("3");
+				systemLog.setException("exception");
+				systemLog.setAuthStatus(1);
+				systemLogService.save(systemLog);
+			}
+			
+		}
+		//删除日志测试方法
+		@Test
+		public void testDeleteSystemLog(){
+			systemLogService.delete(11);
+		}
+			
+		//删除全部日志测试方法
+		@Test
+		public void testDeleteAllSystemLog(){
+			systemLogService.deleteAll();
+		}
+		
+		//更新 日志测试方法
+		@Test
+		public void testUpdateSystemLog(){
+			SystemLog sl = new SystemLog();
+			sl.setId(3);
+			sl.setLogType("警告1");
+			sl.setLogTitle("访问首页1");
+			sl.setRemoteAddr("192.168.1.17");
+			sl.setRequestUri("192.168.1.17");
+			sl.setMethod("post1");
+			sl.setParams("5");
+			sl.setException("exception1");
+			sl.setAuthStatus(2);
+			systemLogService.update(sl);
+		}
+		//查询所有日志测试方法
+		@Test
+		public void testFindAllSystemLog(){
+			List<SystemLog> logs = systemLogService.findAll();
+			for(SystemLog ls: logs){
+				System.out.println(ls.getLogType());
+			}
+		}
+
+}
